2d0167f3bc69eb0157913e49613bd1a0527cf481
guanxiangkai <xiangkaiguan@iCloud.com>
2023-03-11 15:57:08 +0800

    性别枚举名称修改

--- src/main/java/com/rbtxm/springdatajpa/common/constant/Sex.java
+++ src/main/java/com/rbtxm/springdatajpa/common/constant/Sex.java
@@ -4,14 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 /*
- * @Description: 性别枚举
  * @Author: guanxiangkai
+ * @Description: 性别枚举
  * @CreatedTime: 2023-03-10 23:21:23:21
  * @Version: 1.0.0
  */
 @Getter
 @AllArgsConstructor
-public enum Gender {
+public enum Sex {
     MAIE("男"),
     FEMAIE("女");
     private String desc;

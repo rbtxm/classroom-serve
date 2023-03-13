0d171895af83f6ba5c7607917907acf8ea2f81bd
guanxiangkai <xiangkaiguan@iCloud.com>
2023-03-13 19:00:59 +0800

    未完成修改

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,37 +1,6 @@
-plugins {
-    java
-    id("org.springframework.boot") version "3.0.4"
+plugins{
     id("io.spring.dependency-management") version "1.1.0"
 }
 
 group = "com.rbtxm"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
-
-configurations {
-    compileOnly {
-        extendsFrom(configurations.annotationProcessor.get())
-    }
-}
-
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework:spring-jms")
-    implementation("org.springframework.cloud:spring-cloud-context:3.1.3")
-    implementation("org.springframework.cloud:spring-cloud-commons:3.1.3")
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-    implementation("org.springframework:spring-context:5.3.22")
-    runtimeOnly("org.postgresql:postgresql")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    compileOnly("org.projectlombok:lombok")
-    annotationProcessor("org.projectlombok:lombok")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
-}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,17 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
 rootProject.name = "classroom-serve"
+
+include("common")
